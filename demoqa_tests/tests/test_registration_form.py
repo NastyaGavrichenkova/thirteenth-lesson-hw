e4cc395fbf918c0fcaa7a0e038b39fe6928f60be
NastyaGavrichenkova <lady_markos@mail.ru>
2023-09-15 22:06:45 +0800

    add test registration form

---
+++ demoqa_tests/tests/test_registration_form.py
@@ -0,0 +1,11 @@
+import allure
+from demoqa_tests.data import users
+from demoqa_tests.model.registration_page import RegistrationPage
+
+
+@allure.title('Successful registration')
+def test_successful_registration(setup_browser):
+    registration_page = RegistrationPage()
+    registration_page.open()
+    registration_page.registration(users.test_user)
+    registration_page.should_registration_user_with(users.test_user)
